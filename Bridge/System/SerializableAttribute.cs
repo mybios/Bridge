b235b014cd569e71c6202d37197dfe53d3e3f605
mybios@qq.com <mybios@qq.com>
2017-04-05 16:46:52 +0800

    序列化

--- Bridge/System/SerializableAttribute.cs
+++ Bridge/System/SerializableAttribute.cs
@@ -5,8 +5,6 @@ using Bridge;
 namespace System
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
-    [External]
-    [NonScriptable]
     public sealed class SerializableAttribute : Attribute
     {
     }
